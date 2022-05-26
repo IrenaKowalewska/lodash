aeacea8436ef5ebc918e2cfa19f5228cdbe071d4
Your Name <iryna.kavaleuskaya@LeverX.local>
2022-05-26 11:50:37 +0300

    camelCase - added solution

--- camelCase.js
+++ camelCase.js
@@ -17,7 +17,35 @@
  * camelCase('__FOO_BAR__')
  * // => 'fooBar'
  */
-const camelCase = (string) => {}
+const camelCase = (string) => {
+  const cleanString = string.toString()
+    // Replaces any - or _ characters with a space
+    .replace( /[-_]+/g, ' ')
+    // Removes any non alphanumeric characters
+    .replace( /[^\w\s]/g, '')
+    .split("")
+    // Add space after number
+    .map(elem => {
+      return Number(elem) ? `${elem} ` : elem;
+    })
+    .join("");
+
+  if (cleanString.split(" ").length === 1) {
+    const lettersArray = cleanString.split("");
+
+    return lettersArray.map((elem, i) => {
+      return (/[A-Z]/g.test(elem) && lettersArray[i - 1] && lettersArray[i + 1] && (
+        (!/[A-Z]/g.test(lettersArray[i - 1]) && !/[A-Z]/g.test(lettersArray[i + 1]) ) ||
+        (/[A-Z]/g.test(lettersArray[i + 1]) && !/[A-Z]/g.test(lettersArray[i - 1])) ||
+        (/[A-Z]/g.test(lettersArray[i - 1]) && !/[A-Z]/g.test(lettersArray[i + 1]))
+      )) ? elem.toUpperCase() : elem.toLowerCase();
+    }).join("");
+  } else {
+    return cleanString.trim().split(" ").map((elem, i) => {
+      return i === 0 ? elem.toLowerCase() : elem.substr(0, 1).toUpperCase() + elem.substr(1).toLowerCase();
+    }).join("");
+  }
+}
 
 export default camelCase
 
